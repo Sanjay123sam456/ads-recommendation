ce280067af66014d8dcd0f5ff261ea38ba509096
Sanjay kumar <sanjay123sam456@gmail.com>
2025-11-23 18:09:29 +0530

    disabled auto reload

--- app.py
+++ app.py
@@ -336,4 +336,4 @@ def health():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("app:app", host="0.0.0.0", port=PORT, reload=True)
+    uvicorn.run("app:app", host="0.0.0.0", port=PORT)
